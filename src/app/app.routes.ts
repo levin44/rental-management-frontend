35186dc336413a9ac23c1ef7bdcd544969ae2cb9
levin44 <nisaldayan@gmail.com>
2024-07-26 13:21:44 +0530

    home page completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"home",
+        component: HomeComponent
+    },
+    // {
+    //     path:"register",
+    //     component:RegistrationComponent
+    // },
+    {
+        path:"",
+        component: HomeComponent
+    },
+    // {
+    //     path:"view",
+    //     component: ViewComponent
+    // }
+];
