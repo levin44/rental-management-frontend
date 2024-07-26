e63ee2d068beb64d9abbaf5c6453ab59446b2114
levin44 <nisaldayan@gmail.com>
2024-07-26 17:45:24 +0530

    rental

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CustomerComponent } from './pages/customer/customer.component';
 import { ItemComponent } from './pages/item/item.component';
+import { RentalComponent } from './pages/rental/rental.component';
 
 export const routes: Routes = [
     {
@@ -19,5 +20,10 @@ export const routes: Routes = [
     {
         path:"item",
         component: ItemComponent
+    }
+
+    {
+        path:"rental",
+        component: RentalComponent
     }
 ];
