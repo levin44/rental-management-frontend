0ca71b4a62efe207ac4356364c061bfc69db64f5
levin44 <nisaldayan@gmail.com>
2024-07-26 13:52:57 +0530

    customer component completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CustomerComponent } from './pages/customer/customer.component';
 
 export const routes: Routes = [
     {
         path:"home",
         component: HomeComponent
     },
-    // {
-    //     path:"register",
-    //     component:RegistrationComponent
-    // },
+    {
+        path:"customer",
+        component:CustomerComponent
+    },
     {
         path:"",
         component: HomeComponent
